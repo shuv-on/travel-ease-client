5d0755f4d35d051ff03cd7b6eed0ac0791b0ce5d
Md Shishir Kaysar Shuvon <mdshishirkaysarshuvon.ict.mbstu@gmail.com>
2025-11-09 23:04:02 +0600

    Footer error fixed

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,17 +1,19 @@
 
 import React from 'react';
 
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar';
 import Footer from '../../components/Footer/Footer';
 
 
 const Root = () => {
+    const location = useLocation();
+    const showFooter = ['/', '/allvehicles', '/addvehicles', '/myvehicles', '/mybookings'].includes(location.pathname);
     return (
         <div>
             <NavBar></NavBar>
             <Outlet></Outlet>
-            <Footer></Footer>
+           {showFooter && <Footer></Footer>}
         </div>
     );
 };

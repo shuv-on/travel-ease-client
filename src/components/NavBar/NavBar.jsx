f80224cff82cf372b7c7578a9ab23e6e440ee307
Md Shishir Kaysar Shuvon <mdshishirkaysarshuvon.ict.mbstu@gmail.com>
2025-11-09 23:17:21 +0600

    Theme add light or dark

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
-import { NavLink, Link } from 'react-router-dom'; 
+import React, { useState, useEffect } from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
-const NavBar = () => { 
+const NavBar = () => {
+    const [theme, setTheme] = useState('light');
+
+    useEffect(() => {
+        const savedTheme = localStorage.getItem('theme');
+        if (savedTheme) {
+            setTheme(savedTheme);
+            document.documentElement.setAttribute('data-theme', savedTheme);
+        }
+    }, []);
+
+    const toggleTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        setTheme(newTheme);
+        localStorage.setItem('theme', newTheme);
+        document.documentElement.setAttribute('data-theme', newTheme);
+    };
+
     const links = <>
         <li>
             <NavLink
@@ -17,7 +34,7 @@ const NavBar = () => {
             <NavLink
                 to="/allvehicles"
                 className={({ isActive }) =>
-                    `flex items-center gap-2 m-2 hover:border-b-2 hover:border-green-500 ${isActive ? 'border-b-2g border-green-500' : ''}`
+                    `flex items-center gap-2 m-2 hover:border-b-2 hover:border-green-500 ${isActive ? 'border-b-2 border-green-500' : ''}`
                 }
             >
                 All Vehicles
@@ -32,7 +49,6 @@ const NavBar = () => {
             >
                 Add Vehicle
             </NavLink>
-            
         </li>
         <li>
             <NavLink
@@ -41,7 +57,7 @@ const NavBar = () => {
                     `flex items-center gap-2 m-2 hover:border-b-2 hover:border-green-500 ${isActive ? 'border-b-2 border-green-500' : ''}`
                 }
             >
-                My Vehicles 
+                My Vehicles
             </NavLink>
         </li>
         <li>
@@ -56,16 +72,14 @@ const NavBar = () => {
         </li>
     </>;
 
-  
-
     return (
         <div className=''>
             <div className="navbar bg-base-100 shadow-sm px-5">
                 <div className="navbar-start ">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> 
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> 
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
                             </svg>
                         </div>
                         <ul
@@ -89,12 +103,25 @@ const NavBar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end gap-2">
-                  <button className="btn btn-success"><span className='text-white'>Login</span></button>
-                  <button className="btn btn-success"><span className='text-white'>Register</span></button>
+                    <label className="swap swap-rotate">
+                        <input
+                            type="checkbox"
+                            checked={theme === 'dark'}
+                            onChange={toggleTheme}
+                        />
+                        <svg className="swap-on fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,1,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/>
+                        </svg>
+                        <svg className="swap-off fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,0,8.5,12,3.5,3.5,0,0,0,12,15.5Z"/>
+                        </svg>
+                    </label>
+                    <button className="btn btn-success"><span className='text-white'>Login</span></button>
+                    <button className="btn btn-success"><span className='text-white'>Register</span></button>
                 </div>
             </div>
         </div>
     );
-}; 
+};
 
 export default NavBar;

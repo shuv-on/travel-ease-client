4bf788fffe41dc46c594abc36e3dc144463567a3
Md Shishir Kaysar Shuvon <mdshishirkaysarshuvon.ict.mbstu@gmail.com>
2025-11-19 19:03:08 +0600

    Fixed Latest section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Banner from '../../components/Banner/Banner';
+import Latest from '../../components/Latest/Latest';
+import Topcategories from '../../components/Topcategories/Topcategories';
+import FeaturedOwner from '../../components/FeaturedOwner/FeaturedOwner';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Latest></Latest>
+            <Topcategories></Topcategories>
+            <FeaturedOwner></FeaturedOwner>
         </div>
     );
 };
